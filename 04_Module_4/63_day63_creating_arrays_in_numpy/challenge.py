1362cc4a59d37c36f81a962a489968f7cff93dfc
dkishorkoirala <dkishorkoirala@gmail.com>
2025-08-24 17:31:00 +0545

    creating array

---
+++ 04_Module_4/63_day63_creating_arrays_in_numpy/challenge.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# c1
+seq = np.arange(2, 101, 2)
+print(seq)
+
+# c2
+even_ = np.linspace(-5, 5, 50)
+print(even_)
+
+# c3
+ones_ = np.ones((3, 3))
+print(ones_ * 5)
+
+# c4
+
+d1 = np.arange(1, 10)
+reshaped = d1.reshape(3, 3)
+print(reshaped)
---
+++ 04_Module_4/63_day63_creating_arrays_in_numpy/mini_tasks.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+py_list = [10, 20, 30, 40]
+np_array = np.array(py_list)
+
+print(np_array)
+
+# task 2
+arr = np.arange(5, 51, 5)
+print(arr)
+
+# task3
+arr_ = np.linspace(0, 14, 7)
+print(arr_)
+
+# task4
+zeros_ = np.zeros((5, 5))
+print(zeros_)
+
+# task5
+ones_ = np.ones((4, 2))
+print(ones_)
